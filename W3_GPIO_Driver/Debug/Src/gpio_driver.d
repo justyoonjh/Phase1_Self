4e67f193753cb152a80ab57e3d946a2f99ac25af
justyoonjh <juhoyoon0901@gmail.com>
2026-05-09 22:23:33 +0900

    P1W4-2(ledbuttontoggle, finish)

--- W3_GPIO_Driver/Debug/Src/gpio_driver.d
+++ W3_GPIO_Driver/Debug/Src/gpio_driver.d
@@ -1,1 +1,4 @@
-Src/gpio_driver.o: ../Src/gpio_driver.c
+Src/gpio_driver.o: ../Src/gpio_driver.c ../Src/gpio_driver.h \
+ ../Src/gpio_reg.h
+../Src/gpio_driver.h:
+../Src/gpio_reg.h:
--- W3_GPIO_Driver/Debug/Src/main.d
+++ W3_GPIO_Driver/Debug/Src/main.d
@@ -1,2 +1,3 @@
-Src/main.o: ../Src/main.c ../Src/gpio_reg.h
+Src/main.o: ../Src/main.c ../Src/gpio_driver.h ../Src/gpio_reg.h
+../Src/gpio_driver.h:
 ../Src/gpio_reg.h:
