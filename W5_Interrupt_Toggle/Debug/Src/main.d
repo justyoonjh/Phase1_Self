7fb4af2e7c7e476262434d12069c1c84fcefcd74
justyoonjh <juhoyoon0901@gmail.com>
2026-05-15 00:35:55 +0900

    P1W5-4(1) : main.c작성(빌드 안됨)

--- W5_Interrupt_Toggle/Debug/Src/main.d
+++ W5_Interrupt_Toggle/Debug/Src/main.d
@@ -1,1 +1,5 @@
-Src/main.o: ../Src/main.c
+Src/main.o: ../Src/main.c \
+ /home/justjhst/바탕화면/Phase1_Self/W5_Interrupt_Toggle/Drivers/gpio_driver.h \
+ /home/justjhst/바탕화면/Phase1_Self/W5_Interrupt_Toggle/Drivers/gpio_reg.h
+/home/justjhst/바탕화면/Phase1_Self/W5_Interrupt_Toggle/Drivers/gpio_driver.h:
+/home/justjhst/바탕화면/Phase1_Self/W5_Interrupt_Toggle/Drivers/gpio_reg.h:
