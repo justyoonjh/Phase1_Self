530e37f6c593381d811b9215484b61def9b79c6a
justyoonjh <juhoyoon0901@naver.com>
2026-05-18 02:23:40 +0900

    P1W7a-1 : 잠금장치 탑재된 키패드 비밀번호 장치 연결(오류) 키패드 버튼 디바운싱이 안되어 채터링이 심함. 소프트웨어 디바운싱 필요

--- W7_SystickIsHeart/Debug/Src/main.d
+++ W7_SystickIsHeart/Debug/Src/main.d
@@ -1,7 +1,9 @@
 Src/main.o: ../Src/main.c \
  /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/gpio_driver.h \
  /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/gpio_reg.h \
- /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/systick.h
+ /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/systick.h \
+ /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/BSP/keypad_driver.h
 /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/gpio_driver.h:
 /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/gpio_reg.h:
 /home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/Drivers/systick.h:
+/home/justjhst/바탕화면/Phase1_Self/W7_SystickIsHeart/BSP/keypad_driver.h:
