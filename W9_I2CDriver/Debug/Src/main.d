2f92ec18c88cb92e7b43b30c93d3f7a2012e319c
justyoonjh <juhoyoon0901@naver.com>
2026-06-22 17:33:40 +0900

    P1W9-15 : 출력 안됨, 점검 필요

--- W9_I2CDriver/Debug/Src/main.d
+++ W9_I2CDriver/Debug/Src/main.d
@@ -5,6 +5,7 @@ Src/main.o: ../Src/main.c \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/usart_driver.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/BSP/keypad_driver.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/systick.h \
+ /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_scanner.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_driver.h
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/gpio_driver.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/address.h:
@@ -12,4 +13,5 @@ Src/main.o: ../Src/main.c \
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/usart_driver.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/BSP/keypad_driver.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/systick.h:
+/home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_scanner.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_driver.h:
---
+++ W9_I2CDriver/Drivers/Inc/i2c_scanner.h
@@ -0,0 +1,15 @@
+/*
+ * i2c_scanner.h
+ *
+ *  Created on: 2026. 6. 22.
+ *      Author: justjhst
+ */
+
+#ifndef INC_I2C_SCANNER_H_
+#define INC_I2C_SCANNER_H_
+
+#include "i2c_driver.h"
+
+void I2C_AddressScan(I2C_Handle_t *pI2CHandle);
+
+#endif /* INC_I2C_SCANNER_H_ */
