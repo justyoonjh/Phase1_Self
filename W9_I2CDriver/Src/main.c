37c2b336d6a359791c7ffdd0eede3147346454b3
justyoonjh <juhoyoon0901@naver.com>
2026-06-22 16:50:53 +0900

    P1W9-12 : main.c 1차 작업 GPIO AF ON(I2C1), Open-drain, Pull-up

--- W9_I2CDriver/Src/main.c
+++ W9_I2CDriver/Src/main.c
@@ -26,7 +26,19 @@
 
 int main(void)
 {
+	GPIO_Handle_t I2CPins;
+	I2CPins.pGPIOx = GPIOB;
+	I2CPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALT;
+	I2CPins.GPIO_PinConfig.GPIO_PinAltFunMode = 4;
+	I2CPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OPTYPE_OD;
+	I2CPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
+	I2CPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 
+	I2CPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_6;
+	GPIO_Init(&I2CPins);
+
+	I2CPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_7;
+	GPIO_Init(&I2CPins);
 
 	while(1)
 	{
