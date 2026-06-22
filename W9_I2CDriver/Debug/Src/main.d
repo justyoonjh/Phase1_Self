47d02de4783f0d1b4b2cff05f00590f05d2fd312
justyoonjh <juhoyoon0901@naver.com>
2026-06-22 23:33:26 +0900

    P1W9-16 : 기본 실습 1완료 - MPU-6050주소 출력(0x68)완료 - SM 송수신 속도 : 8ms

--- W9_I2CDriver/Debug/Src/main.d
+++ W9_I2CDriver/Debug/Src/main.d
@@ -3,7 +3,6 @@ Src/main.o: ../Src/main.c \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/address.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/rcc_driver.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/usart_driver.h \
- /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/BSP/keypad_driver.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/systick.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_scanner.h \
  /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_driver.h
@@ -11,7 +10,6 @@ Src/main.o: ../Src/main.c \
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/address.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/rcc_driver.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/usart_driver.h:
-/home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/BSP/keypad_driver.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/systick.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_scanner.h:
 /home/justjhst/바탕화면/Phase1_Self/W9_I2CDriver/Drivers/Inc/i2c_driver.h:
